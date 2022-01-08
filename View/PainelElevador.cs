4db8f3a7193203c1f29c1bb32a608995897662aa
Alexsander-Farias <alexsander.devops@hotmail.com>
2022-01-08 17:42:03 -0400

    Adicionando a descrição à classe PainelElevador

--- View/PainelElevador.cs
+++ View/PainelElevador.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace ProjetoElevador.View
 {
+    // Essa classe será responsável por exibir 
+    // as informações do elevador na tela
     internal class PainelElevador
     {
     }
